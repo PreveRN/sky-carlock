3ad178a29381f0a0de98ce51e6fbda93e9103ff1
PreveRN <94971689+PreveRN@users.noreply.github.com>
2025-07-15 18:07:25 +0700

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'SkyInside'
+description 'Car Lock System with ESX, oxmysql, and ox_lib'
+version '2.0.0'
+
+shared_scripts {
+    '@ox_lib/init.lua',
+    'shared/config.lua'
+}
+
+client_scripts {
+    'client/main.lua'
+}
+
+server_scripts {
+    '@oxmysql/lib/MySQL.lua',
+    'server/main.lua'
+}
